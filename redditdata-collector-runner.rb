def62f6ac7b6547a4b7aee6f33ef474b529690d7
mogitfamcap <mogitfamcap@gmail.com>
2014-11-10 21:44:40 -0800

    parse configs

--- redditdata-collector-runner.rb
+++ redditdata-collector-runner.rb
@@ -1,3 +1,24 @@
+require 'parseconfig'
+
 require './util.rb'
 
-Util.log 'redditdata-collector-runner has started'
+def run_session
+  Util.log 'Session has started'
+
+  config = ParseConfig.new('redditdata-collector-runner.config')
+  data_directory = config['data_directory']
+  log_directory = config['log_directory']
+  sleep_between_sessions_seconds = config['sleep_between_sessions_seconds']
+  regex = config['regex']
+
+  Util.log "data_directory=#{data_directory}"
+  Util.log "log_directory=#{log_directory}"
+  Util.log "sleep_between_sessions_seconds=#{sleep_between_sessions_seconds}"
+  Util.log "regex=#{regex}"
+
+  exit 0
+end
+
+while true
+  run_session
+end
