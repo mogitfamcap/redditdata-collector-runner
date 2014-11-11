aa874b998259182e4c2903baa8d27d02897c905a
mogitfamcap <mogitfamcap@gmail.com>
2014-11-10 21:54:14 -0800

    main loop

--- redditdata-collector-runner.rb
+++ redditdata-collector-runner.rb
@@ -16,7 +16,27 @@ def run_session
   Util.log "sleep_between_sessions_seconds=#{sleep_between_sessions_seconds}"
   Util.log "regex=#{regex}"
 
-  exit 0
+  timestamp = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
+  Util.log "timestamp=#{timestamp}"
+
+  static_location = data_directory + 'redditdata.db'
+  temp_location = data_directory + 'redditdata.db' + '.' + timestamp + '.db'
+
+  if File.exist? static_location
+    Util.log "Copying #{static_location} to #{temp_location}"
+    FileUtils.copy static_location, temp_location
+  end
+
+  begin
+
+  rescue StandardError => error
+    Util.log error.message
+    Util.log error.backtrace
+    sleep sleep_between_sessions_seconds.to_i
+  end
+
+  Util.log 'Session has completed'
+  sleep sleep_between_sessions_seconds.to_i
 end
 
 while true
