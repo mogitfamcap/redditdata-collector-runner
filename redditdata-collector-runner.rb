b88a830748ba40488c22f3fc6b83a0dda323c5ae
mogitfamcap <mogitfamcap@gmail.com>
2014-11-13 23:13:56 -0800

    support incremental processing

--- redditdata-collector-runner.rb
+++ redditdata-collector-runner.rb
@@ -3,8 +3,27 @@ require 'fileutils'
 
 require './util.rb'
 
-def collect_dataset(dataset, collector_directory, database_location, logfile, regex)
-  command = "ruby #{collector_directory}bin/redditdata-collector #{database_location} #{dataset} full '#{regex}' >> #{logfile} 2>&1"
+$incremental_count_file = File.dirname(__FILE__) + '/.incremental_runs_since_full'
+
+def get_incremental_count
+  if !File.exist? $incremental_count_file
+    return 0
+  end
+
+  file = File.open($incremental_count_file, 'rb')
+  contents = file.read
+  file.close
+
+  contents.to_i
+end
+
+def set_incremental_count(count)
+  File.open($incremental_count_file, 'w') { |file| file.write(count.to_s) }
+end
+
+
+def collect_dataset(dataset, collector_directory, database_location, logfile, regex, mode)
+  command = "ruby #{collector_directory}bin/redditdata-collector #{database_location} #{dataset} #{mode} '#{regex}' >> #{logfile} 2>&1"
   Util.log "Running command: #{command}"
   exit_code = system(command)
   Util.log "Exit code: #{exit_code}"
@@ -13,6 +32,8 @@ def collect_dataset(dataset, collector_directory, database_location, logfile, re
   end
 
 end
+
+
 
 def run_session
   Util.log 'Session has started'
@@ -23,12 +44,14 @@ def run_session
   log_directory = config['log_directory']
   sleep_between_sessions_seconds = config['sleep_between_sessions_seconds']
   regex = config['regex']
+  incremental_runs_per_full = config['incremental_runs_per_full'].to_i
 
   Util.log "collector_directory=#{collector_directory}"
   Util.log "data_directory=#{data_directory}"
   Util.log "log_directory=#{log_directory}"
   Util.log "sleep_between_sessions_seconds=#{sleep_between_sessions_seconds}"
   Util.log "regex=#{regex}"
+  Util.log "incremental_runs_per_full=#{incremental_runs_per_full}"
 
   timestamp = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
   Util.log "timestamp=#{timestamp}"
@@ -36,16 +59,21 @@ def run_session
   static_location = data_directory + 'redditdata.db'
   temp_location = data_directory + 'redditdata.db' + '.' + timestamp + '.temp' + '.db'
 
+  incremental_runs_since_full = get_incremental_count
+  mode = (incremental_runs_since_full < incremental_runs_per_full) ? 'incremental' : 'full'
+
   if File.exist? static_location
     Util.log "Copying #{static_location} to #{temp_location}"
     FileUtils.copy static_location, temp_location
+  else
+    mode = 'full'
   end
 
   begin
-    collect_dataset('subreddits', collector_directory, temp_location, log_directory + timestamp + '_' + 'subreddits' + '.log', regex)
-    collect_dataset('links', collector_directory, temp_location, log_directory + timestamp + '_' + 'links' + '.log', regex)
-    collect_dataset('users', collector_directory, temp_location, log_directory + timestamp + '_' + 'users' + '.log', regex)
-    collect_dataset('userlinks', collector_directory, temp_location, log_directory + timestamp + '_' + 'userlinks' + '.log', regex)
+    collect_dataset('subreddits', collector_directory, temp_location, log_directory + timestamp + '_' + 'subreddits' + '.log', regex, mode)
+    #collect_dataset('links', collector_directory, temp_location, log_directory + timestamp + '_' + 'links' + '.log', regex, mode)
+    #collect_dataset('users', collector_directory, temp_location, log_directory + timestamp + '_' + 'users' + '.log', regex, mode)
+    #collect_dataset('userlinks', collector_directory, temp_location, log_directory + timestamp + '_' + 'userlinks' + '.log', regex, mode)
 
     Util.log "Copying #{temp_location} to #{static_location}"
     FileUtils.copy temp_location, static_location
@@ -55,6 +83,10 @@ def run_session
 
     Util.log "Removing #{temp_location}"
     FileUtils.rm temp_location
+
+    incremental_runs_since_full = (mode == 'full') ? 0 : incremental_runs_since_full + 1
+    Util.log "Setting incremental_runs_since_full to #{incremental_runs_since_full}"
+    set_incremental_count incremental_runs_since_full
   rescue StandardError => error
     Util.log 'Session has failed'
     Util.log error.message
